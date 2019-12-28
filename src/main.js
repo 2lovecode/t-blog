148dc14459c3fd90e1218ec268a99256d67ff885
liuh31 <liuh31@ziroom.com>
2019-12-28 21:18:49 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import Element from "element-ui"
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/fonts/font.css'
 import routes from "./router"
+import stores from "./store"
+
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -15,8 +17,10 @@ Vue.use(Element, { size: 'small', zIndex: 3000 })
 const router = new VueRouter({
   routes
 })
+const store = new Vuex.Store(stores)
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
