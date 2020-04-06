b32f776a0f739c3d3779cc64738a414ee348b62d
liuhao <liu546hao@163.com>
2020-04-06 12:53:19 +0800

    update

--- src/components/layout/CommonLayout.vue
+++ src/components/layout/CommonLayout.vue
@@ -26,7 +26,7 @@
 export default {
     data () {
         return {
-            appName: "Veen",
+            appName: "2lovecode",
             homePage: "/",
             navList: [
                 {
