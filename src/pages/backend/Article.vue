6942ec127767151edc3a02c8e3b35389351ca8cb
2lovecode <liu546hao@163.com>
2020-09-29 15:36:59 +0800

    feat:backend add article

--- src/pages/backend/Article.vue
+++ src/pages/backend/Article.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="mavonEditor">
-    <mavon-editor 
-    :externalLink="externalLink"
-    v-model="handbook">
-    </mavon-editor>
+    <el-row class="article-form-row">
+      <el-input placeholder="请输入标题" v-model="title">
+        <template slot="prepend">标题</template>
+      </el-input>
+    </el-row>
+    <el-row class="article-form-row">
+      <el-input placeholder="请输入描述" v-model="description">
+        <template slot="prepend">描述</template>
+      </el-input>
+    </el-row>
+    <el-row class="article-form-row">
+      <mavon-editor 
+      :externalLink="externalLink"
+      v-model="content">
+      </mavon-editor>
+    </el-row>
+    <el-row class="article-form-row">
+      <el-button type="info">发布</el-button>
+    </el-row>
   </div>
 </template>
 <script>
@@ -14,7 +29,9 @@ export default {
       markdownOption: {
         bold: true, // 粗体
       },
-      handbook: "#### how to use mavonEditor in nuxt.js"
+      title: "",
+      description: "",
+      content: "#### how to use mavonEditor in nuxt.js"
     };
   }
 };
@@ -24,5 +41,8 @@ export default {
 .mavonEditor {
   width: 80%;
   height: 80%;
+}
+.article-form-row{
+  margin-bottom: 1em;
 }
 </style>
